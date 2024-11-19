5522f06c7287e27d7d129b4f59a444166f2c088f
Spencer Miller <spence.miller@gmail.com>
2024-11-18 17:09:31 -0800

    descriptor allocator and layout builder

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,75 @@
 ﻿#include <vk_descriptors.h>
+
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type)
+{
+	VkDescriptorSetLayoutBinding newBinding{};
+	newBinding.binding = binding;
+	newBinding.descriptorCount = 1;
+	newBinding.descriptorType = type;
+	bindings.push_back(newBinding);
+}
+
+void DescriptorLayoutBuilder::clear()
+{
+	bindings.clear();
+}
+
+VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shaderStages, void* pNext, VkDescriptorSetLayoutCreateFlags flags)
+{
+	for (auto& b : bindings) {
+		b.stageFlags |= shaderStages;
+	}
+
+	VkDescriptorSetLayoutCreateInfo info {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO };
+	info.pNext = pNext;
+	info.pBindings = bindings.data();
+	info.bindingCount = uint32_t(bindings.size());
+	info.flags = flags;
+
+	VkDescriptorSetLayout set;
+	VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+
+	return set;
+}
+
+void DescriptorAllocator::init_pool(VkDevice device, uint32_t maxSets, std::span<PoolSizeRatio> poolRatios)
+{
+	std::vector<VkDescriptorPoolSize> sizes;
+	for (PoolSizeRatio poolrat : poolRatios) {
+		sizes.push_back(VkDescriptorPoolSize{
+			.type = poolrat.type,
+			.descriptorCount = uint32_t(maxSets * poolrat.ratio),
+			});
+	}
+
+	VkDescriptorPoolCreateInfo info{.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO };
+	info.flags = 0;
+	info.maxSets = maxSets;
+	info.poolSizeCount = uint32_t(sizes.size());
+	info.pPoolSizes = sizes.data();
+
+	vkCreateDescriptorPool(device, &info, nullptr, &pool);
+}
+
+void DescriptorAllocator::clear_descriptors(VkDevice device)
+{
+	vkResetDescriptorPool(device, pool, 0);
+}
+
+void DescriptorAllocator::destroy_pool(VkDevice device)
+{
+	vkDestroyDescriptorPool(device, pool, nullptr);
+}
+
+VkDescriptorSet DescriptorAllocator::allocate(VkDevice device, VkDescriptorSetLayout layout)
+{
+	VkDescriptorSetAllocateInfo allocInfo{ .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO };
+	allocInfo.descriptorPool = pool;
+	allocInfo.descriptorSetCount = 1;
+	allocInfo.pSetLayouts = &layout;
+
+	VkDescriptorSet ds;
+	VK_CHECK(vkAllocateDescriptorSets(device, &allocInfo, &ds));
+
+	return ds;
+}
